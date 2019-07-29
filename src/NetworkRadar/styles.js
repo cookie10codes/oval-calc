0cc28a525bb9c04ada8c001e1225ea3e79302a03
Stefanie Gevaert <stefanie@koeketienedesign.be>
2019-07-29 16:36:51 +0200

    cleaned files

--- src/NetworkRadar/styles.js
+++ src/NetworkRadar/styles.js
@@ -1,13 +1,15 @@
 import styled, { css } from "styled-components";
 
+/*https://css-tricks.com/snippets/sass/placing-items-circle/*/
+
 function createCSS(itemCount, x, y) {
-  let styles = "";
+    let styles = "";
 
-  let angle = 360 / itemCount;
-  let rot = 0;
+    let angle = 360 / itemCount;
+    let rot = 0;
 
-  for (let i = 0; i < itemCount; i += 1) {
-    styles += `
+    for (let i = 0; i < itemCount; i += 1) {
+        styles += `
               &:nth-of-type(${i + 1}) {
                 transform:
                   rotate(${rot * 1}deg)
@@ -15,68 +17,47 @@ function createCSS(itemCount, x, y) {
                   rotate(${rot * -1}deg);
               }
          `;
-    rot = rot + angle;
-  }
+        rot = rot + angle;
+    }
 
-  console.log(styles);
+    console.log(styles);
 
-  return css`
-    ${styles}
-  `;
+    return css`
+        ${styles}
+    `;
 }
 
 export const NetworkRadarContainer = styled.div`
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-  .radar-circle {
-    position: absolute;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    border: 1px solid rgba(0, 0, 0, 0.1);
-    border-radius: 100%;
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    .radar-circle {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        border-radius: 100%;
 
-    @media (min-width: 769px) {
-      top: 50%;
+        /* @media (min-width: 769px) {
+            top: 50%;
+        } */
     }
-  }
 
-  .radar-person {
-    width: 80px;
-    height: 100px;
-    background: rgba(255, 0, 0, 0.3);
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    position: absolute;
-  }
-`;
-
-export const Circle = styled.div`
-  /*https://css-tricks.com/snippets/sass/placing-items-circle/*/
-  position: relative;
-  width: 45vw;
-  height: 40vh;
-  padding: 0;
-  border-radius: 50%;
-  list-style: none;
-
-  > * {
-    display: block;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 80px;
-    height: 100px;
-    margin: -40px;
-    background: red;
-
-    ${props => createCSS(props.itemCount)};
-  }
+    .radar-person {
+        width: 80px;
+        height: 100px;
+        background: rgba(255, 0, 0, 0.3);
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        margin: -40px;
+        position: absolute;
+    }
 `;
 
 export const RadarCloseFamily = styled.div`
-  /* width: 20vw;
+    /* width: 20vw;
   height: 20vh;
   z-index: 4;
 
@@ -84,28 +65,15 @@ export const RadarCloseFamily = styled.div`
     width: 25vw;
     height: 20vh;
   } */
-  position: relative;
-  width: 20vw;
-  height: 20vh;
-  padding: 0;
-  border-radius: 50%;
-  list-style: none;
+    width: 20vw;
+    height: 20vh;
 
-  > * {
-    display: block;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 80px;
-    height: 100px;
-    margin: -40px;
-    background: red;
-
-    ${props => createCSS(props.itemCount, 10, 10)};
-  }
+    > * {
+        ${props => createCSS(props.itemCount, 10, 10)};
+    }
 `;
 export const RadarFamily = styled.div`
-  /* width: 45vw;
+    /* width: 45vw;
   height: 45vh;
   z-index: 3;
 
@@ -113,28 +81,15 @@ export const RadarFamily = styled.div`
     width: 45vw;
     height: 40vh;
   } */
-  position: relative;
-  width: 40vw;
-  height: 40vh;
-  padding: 0;
-  border-radius: 50%;
-  list-style: none;
+    width: 40vw;
+    height: 40vh;
 
-  > * {
-    display: block;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 80px;
-    height: 100px;
-    margin: -40px;
-    background: red;
-
-    ${props => createCSS(props.itemCount, 20, 20)};
-  }
+    > * {
+        ${props => createCSS(props.itemCount, 20, 20)};
+    }
 `;
 export const RadarFriends = styled.div`
-  /* width: 70vw;
+    /* width: 70vw;
   height: 70vh;
   z-index: 2;
 
@@ -142,28 +97,15 @@ export const RadarFriends = styled.div`
     width: 70vw;
     height: 65vh;
   } */
-  position: relative;
-  width: 70vw;
-  height: 70vh;
-  padding: 0;
-  border-radius: 50%;
-  list-style: none;
+    width: 70vw;
+    height: 70vh;
 
-  > * {
-    display: block;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 80px;
-    height: 100px;
-    margin: -40px;
-    background: red;
-
-    ${props => createCSS(props.itemCount, 35, 35)};
-  }
+    > * {
+        ${props => createCSS(props.itemCount, 35, 35)};
+    }
 `;
 export const RadarOther = styled.div`
-  /* width: 95vw;
+    /* width: 95vw;
   height: 95vh;
   z-index: 1;
 
@@ -171,39 +113,12 @@ export const RadarOther = styled.div`
     width: 95vw;
     height: 90vh;
   } */
-  position: relative;
-  width: 90vw;
-  height: 90vh;
-  padding: 0;
-  border-radius: 50%;
-  list-style: none;
-
-  > * {
-    display: block;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 80px;
-    height: 100px;
-    margin: -40px;
-    background: red;
-
-    ${props => createCSS(props.itemCount, 45, 45)};
-  }
-`;
-
-export const RadarGroup = styled.div`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  width: 75vw;
-  height: 75vw;
-  transform: translate(-50%, -50%);
-
-  @media (min-width: 769px) {
-    width: 95vw;
+    width: 90vw;
     height: 90vh;
-  }
+
+    > * {
+        ${props => createCSS(props.itemCount, 45, 45)};
+    }
 `;
 
 export default NetworkRadarContainer;
